b97eaf99b8f0b397914802ceedb4307e5d0e2090
Matt Monroe <matt.a.monroe@gmail.com>
2023-01-06 09:56:40 -0800

    add DATE parameter for vod downloads

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM nixos/nix
 
+RUN nix-channel --update
 RUN nix-env -f '<nixpkgs>' -iA youtube-dl bash
 
 VOLUME /mnt/vods
 
 ENV LINK "https://twitch.tv/Laserath"
 ENV COOLDOWN "60"
+ENV DATE ""
 
 RUN mkdir -p /root/cwd
 ADD runService.sh /root/cwd
--- runService.sh
+++ runService.sh
@@ -4,15 +4,22 @@
 # start youtube-dl trying to save a currently running stream at LINK
 # if it isn't live, or when it ends, sleep COOLDOWN seconds and try again
 #
-
+# if DATE is supplied, we will use that with --dateafter to filter the 
+# videos to download
+#
 
 youtube-dl -U
+
+OPT=""
+if [ "x${DATE}x" != "xx" ]; then
+	OPT="--dateafter ${DATE} "
+fi
 
 pushd /mnt/vods
 while true
 do
-	echo "checking for stream"
-	youtube-dl ${LINK}
+	echo "checking for stream (youtube-dl version: `youtube-dl --version`)"
+	youtube-dl ${OPT}${LINK}
 	echo "youtube-dl exit, sleep for ${COOLDOWN} seconds"
 	sleep ${COOLDOWN}s
 done
